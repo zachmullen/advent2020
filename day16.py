aaed602b8504c601a496a07443b492d53f7b4718
Zach Mullen <zach.mullen@kitware.com>
2020-12-16 20:32:05 -0500

    Day 16 (revised)
    It turns out, at least for my dataset, that a greedy approach works.
    The naive backtracking solution is preserved in history for those who
    wish to use their computer as a resistive heater.

--- day16.py
+++ day16.py
@@ -1,4 +1,3 @@
-import functools
 import math
 import re
 
@@ -14,42 +13,35 @@ for i, line in enumerate(lines):
     name, l1, u1, l2, u2 = match.groups()
     ranges[name] = ((int(l1), int(u1)), (int(l2), int(u2)))
 
+my_ticket = list(map(int, (v for v in lines[i + 2].split(','))))
+fields = list(ranges.keys())
+valid_tickets = []
+
 def valid_ticket(vals: list) -> bool:
     for v in vals:
         if not any(r1[0] <= v <= r1[1] or r2[0] <= v <= r2[1] for r1, r2 in ranges.values()):
             return False
     return True
 
-my_ticket = list(map(int, (v for v in lines[i + 2].split(','))))
-all_fields = set(ranges.keys())
-all_indexes = set(range(len(all_fields)))
-valid_tickets = []
-positions = {}
-
 for line in lines[i + 5:]:
     ticket = list(map(int, (v for v in line.split(','))))
     if valid_ticket(ticket):
         valid_tickets.append(ticket)
 
-@functools.lru_cache(maxsize=None)
 def is_valid(field: str, i: int) -> bool:
     r1, r2 = ranges[field]
     return all(r1[0] <= t[i] <= r1[1] or r2[0] <= t[i] <= r2[1] for t in valid_tickets)
 
-def backtrack_solve() -> bool:
-    missing = all_fields - set(positions.keys())
-    if not missing:
-        return True
-    field = next(iter(missing))
-    valid_indexes = all_indexes - set(positions.values())
-    for i in valid_indexes:
-        if is_valid(field, i):
-            positions[field] = i
-            if backtrack_solve():
-                return True
-            positions.pop(field)
-    return False
+ordered = [None] * len(fields)
+opts = [{f for f in fields if is_valid(f, i)} for i in range(len(fields))]
 
-backtrack_solve()
-indexes = [v for k, v in positions.items() if k.startswith('departure')]
+while None in ordered:  # assumes greedy approach works
+    for i, allowed in enumerate(opts):
+        if len(allowed) == 1:
+            ordered[i] = list(allowed)[0]
+            for j, allowed in enumerate(opts):
+                allowed -= {ordered[i]}
+            break
+
+indexes = [i for i, v in enumerate(ordered) if v.startswith('departure')]
 print(math.prod(my_ticket[i] for i in indexes))
